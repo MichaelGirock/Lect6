6e97de019dee989463ea23b06a31ebf0ed8391c3
EC2 Default User <ec2-user@ip-172-31-21-205.ec2.internal>
2021-02-08 17:44:35 +0000

    HTML&CSS added, not pretty, still need demo

--- Project1/app.py
+++ Project1/app.py
@@ -1,6 +1,9 @@
 import requests
 import os
 from dotenv import load_dotenv, find_dotenv
+from flask import Flask, render_template 
+
+app = Flask(__name__)
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 BASE_URL = 'https://api.spotify.com/v1/artists/3TVXtAsR1Inumwj472S9r4/top-tracks'
@@ -34,3 +37,33 @@ print(output['tracks'][0]['album']['name'])
 print(output['tracks'][0]['artists'][0]['name'])
 print(output['tracks'][0]['album']['images'][0]['url'])
 
+song =(output['tracks'][0]['album']['name'])
+artist =(output['tracks'][0]['artists'][0]['name'])
+cover = (output['tracks'][0]['album']['images'][0]['url'])
+
+
+# HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][] HTML BELOW [][][][][]
+
+
+
+
+#blah =["https://i.scdn.co/image/ab67616d0000b27352c75ed37313b889447011ef"]
+
+@app.route('/')
+def spotifyAPI():
+    print('HTML IN PROGRESS')
+    
+    return render_template(
+        "indexs.html",
+        songlen=len(song),
+        song=song,
+        artistlen=len(artist),
+        artist=artist,
+        coverlen=len(cover),
+        cover=cover)
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
