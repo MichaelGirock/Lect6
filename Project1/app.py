3cc05e60d4614bbfc0f2027d6c59c427cf5e996e
EC2 Default User <ec2-user@ip-172-31-21-205.ec2.internal>
2021-02-08 16:18:34 +0000

    First Trial, trying drakes URL

---
+++ Project1/app.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4'
+
+load_dotenv(find_dotenv())
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': os.getenv('CLIENT_ID'),
+    'client_secret': os.getenv('CLIENT_SECRET'),
+})
+
+
+auth_response_data = auth_response.json()#make data JSON
+
+access_token = auth_response_data['access_token']#uses access token
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)#Not 100% sure on this line
+}
+
+r = requests.get(BASE_URL, 
+                headers=headers, 
+                params={'include_groups': 'album', 'limit': 10})
+
+
+output = r.json()
+
+
+for i in range (0,10):
+    print(output['albums']['items'][i]['name'])
